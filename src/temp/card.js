e1c6ca8e6df6d23d41ca78c5862ce0ecd130fd96
李传浩 <ALNY-AL@outlook.com>
2019-06-13 13:26:41 +0800

    添加组件模板

---
+++ src/temp/card.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+/**卡片式组件类 */
+export default class $NAME$ extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+        }
+    }
+    /**渲染函数 */
+    render() {
+        return (
+            <View style={styles.panel}>
+                <View style={styles.header}>
+                </View>
+                <View style={styles.body}>
+                    <Text>$NAME$</Text>
+                </View>
+                <View style={styles.footer}>
+                </View>
+            </View>
+        );
+    }
+};
+
+
+/**样式 */
+const styles = StyleSheet.create({
+    /**外壳 */
+    panel: {
+    },
+    /**头部 */
+    header: {
+    },
+    /**内容区 */
+    body: {
+    },
+    /**页脚 */
+    footer: {
+    }
+});
--- src/temp/default.js
+++ src/temp/default.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-/**组件类 */
+/**基础组件类 */
 export default class $NAME$ extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: '',
+
         }
     }
     /**渲染函数 */
     render() {
         return (
-            <View style={styles.panel}>
-                <View style={styles.header}>
-                </View>
-                <View style={styles.body}>
-                    <Text>$NAME$</Text>
-                </View>
-                <View style={styles.footer}>
-                </View>
-            </View>
+            <View>$NAME$</View>
         );
     }
 };
@@ -28,16 +20,5 @@ export default class $NAME$ extends Component {
 
 /**样式 */
 const styles = StyleSheet.create({
-    /**外壳 */
-    panel: {
-    },
-    /**头部 */
-    header: {
-    },
-    /**内容区 */
-    body: {
-    },
-    /**页脚 */
-    footer: {
-    }
+
 });
